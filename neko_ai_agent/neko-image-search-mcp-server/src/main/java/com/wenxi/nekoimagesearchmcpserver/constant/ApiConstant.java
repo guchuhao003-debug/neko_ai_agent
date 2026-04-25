7cd0a6d8c6cef5a7f442357a4e7bbf9ed6c90c96
guchuhao003-debug <guchuhao003@gmail.com>
2026-04-25 21:29:04 +0800

    ImageSearchTool 图片搜索服务、Github文档加载器

--- neko_ai_agent/neko-image-search-mcp-server/src/main/java/com/wenxi/nekoimagesearchmcpserver/constant/ApiConstant.java
+++ neko_ai_agent/neko-image-search-mcp-server/src/main/java/com/wenxi/nekoimagesearchmcpserver/constant/ApiConstant.java
@@ -8,6 +8,6 @@ public interface ApiConstant {
     /**
      * Pexels API 密钥
      */
-    String PexelsApiKey = "SAOuBQ1hJ5zAuhp8g3mnKQj1RojtJzRLsxmyWuMM1PqVkXkPVRCcA8Ph";
+    String PexelsApiKey = "#########################################";
 
 }
--- neko_ai_agent/neko-image-search-mcp-server/src/main/java/com/wenxi/nekoimagesearchmcpserver/tools/ImageSearchTool.java
+++ neko_ai_agent/neko-image-search-mcp-server/src/main/java/com/wenxi/nekoimagesearchmcpserver/tools/ImageSearchTool.java
@@ -21,7 +21,6 @@ import java.util.stream.Collectors;
 public class ImageSearchTool {
 
     // 替换为你的 Pexels API 密钥（需从官网申请）
-    //private static final String API_KEY = "SAOuBQ1hJ5zAuhp8g3mnKQj1RojtJzRLsxmyWuMM1PqVkXkPVRCcA8Ph";
     private final String API_KEY = ApiConstant.PexelsApiKey;
 
     // Pexels 常规搜索接口（请以文档为准）
